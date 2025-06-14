72a188ec3f0f47f488a349c0a9f76da507419f77
Sam M <sam.m@saluscyber.com>
2025-06-14 21:49:03 +0100

    Add more fields to admin

--- src/earthlings_on_mars_foundation/calls/admin.py
+++ src/earthlings_on_mars_foundation/calls/admin.py
@@ -7,15 +7,34 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Recruit)
-admin.site.register(models.NPC)
 admin.site.register(models.Location)
 admin.site.register(models.RecruitMission)
-admin.site.register(models.MissionPrerequisites)
-admin.site.register(models.CallLog)
+admin.site.register(models.MissionPrerequisite)
+
+class NPCAdmin(admin.ModelAdmin):
+    list_display: ClassVar[list[str]] = ["name", "extension"]
+    search_fields: ClassVar[list[str]] = ["name", "extension"]
+
+admin.site.register(models.NPC, NPCAdmin)
+
+class RecruitNPCInline(admin.TabularInline):
+    verbose_name = "NPC Relationships"
+    model = models.RecruitNPC
+    fk_name = "recruit"
+    extra = 0
+
+class RecruitAdmin(admin.ModelAdmin):
+    inlines = [RecruitNPCInline]
+
+admin.site.register(models.Recruit, RecruitAdmin)
+
+class CallLogAdmin(admin.ModelAdmin):
+    list_display = ["recruit", "NPC", "location", "date", "duration"]
+
+admin.site.register(models.CallLog, CallLogAdmin)
 
 class PrerequisiteInline(admin.TabularInline):
-    model = models.MissionPrerequisites
+    model = models.MissionPrerequisite
     fk_name = "mission"
     extra = 1
 
@@ -42,7 +61,10 @@ class MissionAdmin(admin.ModelAdmin):
                 ]
             }
         ),
-        ("Time Limitations", {"fields": ["not_before", "not_after", "cancel_after_time", "cancel_after_tries", "cancel_text"]}),
+        ("Time Limitations", {
+            "fields": ["not_before", "not_after", "cancel_after_time", "cancel_after_tries", "cancel_text"],
+            "description": "These settings affect when the mission can be given to a recruit"
+        }),
         ("Call from a specific location", {"fields": ["call_back_from"], "classes": ["collapse"]}),
         ("Call another NPC", {"fields": ["call_another"], "classes": ["collapse"]}),
         ("Call back with a code from a physical item", {"fields": ["code"], "classes": ["collapse"]})
